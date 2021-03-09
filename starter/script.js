a9df1710b3a8cc3e2fda758875fc09ac2f5b6e99
prajapatilalit <prajapatilalit150992@gmail.com>
2021-03-09 16:35:21 +0530

    update tip calculator using array

--- starter/script.js
+++ starter/script.js
@@ -82,33 +82,58 @@ HINT: To check if number A is at least double number B, check for A >= 2 * B. Ap
 GOOD LUCK 😀
 */
 
-const calcAverage = (a, b, c) => (a + b + c) / 3;
+// const calcAverage = (a, b, c) => (a + b + c) / 3;
 
-// data 1 dolphin=(44, 23, 71) and koalas (65, 54, 49)
-let avgDolphinScore = calcAverage(44, 23, 71);
-let avgKoalasScore = calcAverage(65, 54, 49);
+// // data 1 dolphin=(44, 23, 71) and koalas (65, 54, 49)
+// let avgDolphinScore = calcAverage(44, 23, 71);
+// let avgKoalasScore = calcAverage(65, 54, 49);
 
-console.log(
-  "avgDolphinScore:",
-  avgDolphinScore,
-  "avgKoalasScore:",
-  avgKoalasScore
-);
+// console.log(
+//   "avgDolphinScore:",
+//   avgDolphinScore,
+//   "avgKoalasScore:",
+//   avgKoalasScore
+// );
 
-const checkWinner = (avgDolphin, avgKoalas) => {
-  if (avgDolphin >= 2 * avgKoalas) {
-    console.log(`Dolphin Win 🏆(${avgDolphin} Vs. ${avgKoalas})`);
-  } else if (avgKoalas >= 2 * avgDolphin) {
-    console.log(`Koalas Win 🏆(${avgKoalas} Vs. ${avgDolphin})`);
-  } else {
-    console.log("No Team Win");
-  }
-};
+// const checkWinner = (avgDolphin, avgKoalas) => {
+//   if (avgDolphin >= 2 * avgKoalas) {
+//     console.log(`Dolphin Win 🏆(${avgDolphin} Vs. ${avgKoalas})`);
+//   } else if (avgKoalas >= 2 * avgDolphin) {
+//     console.log(`Koalas Win 🏆(${avgKoalas} Vs. ${avgDolphin})`);
+//   } else {
+//     console.log("No Team Win");
+//   }
+// };
 
-checkWinner(avgDolphinScore, avgKoalasScore);
+// checkWinner(avgDolphinScore, avgKoalasScore);
 
-//data 2 dolphin=(85, 54, 41) and koalas (23, 34, 27)
-avgDolphinScore = calcAverage(85, 54, 41);
-avgKoalasScore = calcAverage(23, 34, 27);
+// //data 2 dolphin=(85, 54, 41) and koalas (23, 34, 27)
+// avgDolphinScore = calcAverage(85, 54, 41);
+// avgKoalasScore = calcAverage(23, 34, 27);
 
-checkWinner(avgDolphinScore, avgKoalasScore);
+// checkWinner(avgDolphinScore, avgKoalasScore);
+
+// Coding Challenge #2
+
+/*
+Ramesh is still building his tip calculator, using the same rules as before: Tip 15% of the bill if the bill value is between 50 and 300, and if the value is different, the tip is 20%.
+
+1. Write a function 'calcTip' that takes any bill value as an input and returns the corresponding tip, calculated based on the rules above (you can check out the code from first tip calculator challenge if you need to). Use the function type you like the most. Test the function using a bill value of 100.
+2. And now let's use arrays! So create an array 'bills' containing the test data below.
+3. Create an array 'tips' containing the tip value for each bill, calculated from the function you created before.
+4. Create an array 'total' containing the total values, so the bill + tip.
+
+TEST DATA: 125, 555 and 44
+
+HINT: Remember that an array needs a value in each position, and that value can actually be the returned value of a function! So you can just call a function as array values (so don't store the tip values in separate variables first, but right in the new array) 😉
+
+GOOD LUCK 😀*/
+
+const calcTip = (bill) =>
+  bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+
+console.log(bills, tips, totals);
