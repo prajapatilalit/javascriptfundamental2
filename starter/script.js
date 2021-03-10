de6eed391b75c01e0ef996716ef34d71da68d2ad
prajapatilalit <prajapatilalit150992@gmail.com>
2021-03-10 09:50:27 +0530

    completed basic fundamental of javaScripts

--- starter/script.js
+++ starter/script.js
@@ -321,3 +321,34 @@ const lalit = [
 for (let i = lalit.length - 1; i >= 0; i--) {
   console.log(lalit[i]);
 }
+
+//coding challenge 4
+
+const calcTip = (bill) =>
+  bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+
+const bills = [22, 295, 176, 440, 37, 185, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
+
+for (let i = 0; i < bills.length; i++) {
+  const tip = calcTip(bills[i]);
+  tips.push(tip);
+  totals.push(bills[i] + tip);
+}
+
+console.log(tips);
+console.log(totals);
+
+const calcAverage = function (arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    //  sum = sum+arr[i];
+    sum += arr[i];
+  }
+  return sum / arr.length;
+};
+
+console.log(calcAverage(bills));
+console.log(calcAverage(tips));
+console.log(calcAverage(totals));
